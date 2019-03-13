50d449dfe568327e6a1aba7489d8767d19849241
k24koba <hozu.kazu.hozu@gmail.com>
2019-03-13 18:49:27 +0900

    modified functions

--- scripts/torque_control.py
+++ scripts/torque_control.py
@@ -7,16 +7,15 @@ from kondo_b3mservo_rosdriver.msg import servo_command
 import Kondo_B3M_functions as Kondo_B3M
 
 
-ser = serial.Serial('/dev/Kondo_USB-RS485_converter', 1500000)
+ser = serial.Serial('/dev/Kondo', 1500000)
 time.sleep(0.1)
 
-Kondo_B3M.resetServo(4)
 Kondo_B3M.enFreeServo(4)
 Kondo_B3M.change_servocontrol_mode(4, 8) #mode : 00>positionCTRL, 04>velocityCTRL, 08>current(torque)CTRL, 12>feedforwardCTRL
 
-def torque_control(servo_command):
+def position_control(servo_command):
     Kondo_B3M.control_servo_by_Torque(4, servo_command.target_torque)
-    #Kondo_B3M.get_servo_Current(4)
+    
 
 def enfree_servo_after_node_ends(signal, frame):
     Kondo_B3M.enFreeServo(4)
@@ -25,9 +24,9 @@ def enfree_servo_after_node_ends(signal, frame):
 signal.signal(signal.SIGINT, enfree_servo_after_node_ends)
 
 if __name__ == '__main__':
-    rospy.init_node('torque_control')
-    rospy.Subscriber('command', servo_command, torque_control, queue_size = 1)
-    rate = rospy.Rate(100)
+    rospy.init_node('position_control')
+    rospy.Subscriber('command', servo_command, position_control)
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         rate.sleep()
     rospy.spin()
