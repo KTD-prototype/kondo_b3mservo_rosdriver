b986877e737289f99ab721381f6ec5210f07179e
humanrobot_koba <hozu.kazu.hozu@gmail.com>
2019-03-18 02:15:24 +0900

    examined loop time

--- scripts/torque_control.py
+++ scripts/torque_control.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#code for python2
+# code for python2
 import rospy
-import serial, time, signal, sys
+import serial
+import time
+import signal
+import sys
 from kondo_b3mservo_rosdriver.msg import servo_command
 import Kondo_B3M_functions as Kondo_B3M
 
@@ -11,22 +14,25 @@ ser = serial.Serial('/dev/Kondo_USB-RS485_converter', 1500000)
 time.sleep(0.1)
 
 pre_target_torque = 0
-MINIMUM_STEP_OF_TARGET_TORQUE = 225
+MINIMUM_STEP_OF_TARGET_TORQUE = 200
 
 Kondo_B3M.resetServo(4)
 Kondo_B3M.enFreeServo(4)
 Kondo_B3M.reset_encoder_total_count(4)
-Kondo_B3M.change_servocontrol_mode(4, 8) #mode : 00>positionCTRL, 04>velocityCTRL, 08>current(torque)CTRL, 12>feedforwardCTRL
+# mode : 00>positionCTRL, 04>velocityCTRL, 08>current(torque)CTRL, 12>feedforwardCTRL
+Kondo_B3M.change_servocontrol_mode(4, 8)
+
 
 def torque_control(servo_command):
     global pre_target_torque
     target_torque = servo_command.target_torque
-
-    #damp target torque since drastic difference of target torque may cause lock of servo
+    # damp target torque since drastic difference of target torque may cause lock of servo
     target_torque = damp_target_torque(target_torque, pre_target_torque)
 
     Kondo_B3M.control_servo_by_Torque(4, target_torque)
+    # print(str(time.time()))
     Kondo_B3M.get_encoder_total_count(4)
+
     pre_target_torque = target_torque
 
 
@@ -42,12 +48,13 @@ def enfree_servo_after_node_ends(signal, frame):
     Kondo_B3M.enFreeServo(4)
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, enfree_servo_after_node_ends)
 
 
 if __name__ == '__main__':
     rospy.init_node('torque_control')
-    rospy.Subscriber('command', servo_command, torque_control, queue_size = 1)
+    rospy.Subscriber('command', servo_command, torque_control, queue_size=1)
     # rate = rospy.Rate(100)
     # while not rospy.is_shutdown():
     #     rate.sleep()
