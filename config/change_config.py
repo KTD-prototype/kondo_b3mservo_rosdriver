175f21cd6121c18ccc89d30f703a633bc1c99992
k24koba_desktop <hozu.kazu.hozu@gmail.com>
2019-07-14 19:04:07 +0900

    cleaned

--- config/change_config.py
+++ config/change_config.py
@@ -32,11 +32,11 @@ def change_config():
     # Config.change_current_limit(3, 10000)
     # Config.change_current_limit(4, 10000)
 
-    # Config.change_preset_gain(3, 0x6e, 20000)  # P gain for vel
-    # Config.change_preset_gain(3, 0x72, 200)  # D gain for vel
-    # Config.change_preset_gain(3, 0x76, 8000)  # I gain for vel
-    # Config.change_preset_gain(3, 0x7a, 0)  # stat friction for vel
-    # Config.change_preset_gain(3, 0x7c, 0)  # dyna friction for vel
+    Config.change_preset_gain(3, 0x6e, 20000)  # P gain for vel
+    Config.change_preset_gain(3, 0x72, 200)  # D gain for vel
+    Config.change_preset_gain(3, 0x76, 8000)  # I gain for vel
+    Config.change_preset_gain(3, 0x7a, 0)  # stat friction for vel
+    Config.change_preset_gain(3, 0x7c, 100)  # dyna friction for vel
 
     # Config.change_preset_gain(3, 0x7e, 5)  # P gain for tor
     # Config.change_preset_gain(3, 0x82, 0)  # D gain for tor
